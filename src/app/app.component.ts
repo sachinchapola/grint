2935cedd4d78e99662779b35e186df3a33d6c08e
sachinchapola <sachinchapola@gmail.com>
2024-08-26 15:50:59 +0530

    add landing section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeContentComponent } from "./landing-section/home-content/home-content.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
